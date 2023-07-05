84df29391cd0390310c8d2a425e69bf4166b1aff
gzz100 <gzz0081@sina.com>
2023-07-05 18:13:36 +0800

    change code

--- debug.php
+++ debug.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
 if(!isset($_SESSION['user']) || empty($_SESSION['data'])) exit;
-echo json_encode($_SESSION['data']);
+
+function unicode2Chinese($str)
+{
+    return preg_replace_callback("#\\\u([0-9a-f]{4})#i",
+        function ($r) {return iconv('UCS-2BE', 'UTF-8', pack('H4', $r[1]));},
+        $str);
+}
+
+echo unicode2Chinese(json_encode($_SESSION['data']));
 ?>
